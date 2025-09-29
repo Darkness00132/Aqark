ca42df8354f5a5fb23ebe06b467ea62ba0072282
Darkness00132 <mustafa54862@gmail.com>
2025-09-30 00:14:29 +0300

    first commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -70,18 +70,6 @@ export default function RootLayout({
       className={geistCairo.className}
       dir="rtl"
     >
-      <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
-          strategy="lazyOnload"
-        />
-        <Script id="google-analytics" strategy="lazyOnload">{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-XXXXXXXXXX');
-`}</Script>
-      </head>
       <body className="antialiased bg-gradient-to-bl from-primary/30 via-base-100 to-secondary/30 overflow-x-hidden min-h-screen">
         <Provider>
           <Header />
