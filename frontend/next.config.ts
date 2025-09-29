0e3f55d4345ece110598c072e57df9b22e5642c9
Darkness00132 <mustafa54862@gmail.com>
2025-09-29 19:23:01 +0300

    first commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,15 +11,20 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "picsum.photos",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
         hostname: "aqark-s3.s3.us-east-1.amazonaws.com",
         pathname: "/**",
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "www.aqark.vercel.app" }],
+        destination: "https://aqark.vercel.app/:1",
+        permanent: true,
+      },
+    ];
   },
   modularizeImports: {
     "react-icons": {
