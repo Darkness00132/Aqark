8cc2fbacb1c7cb3a3936cfda158b95d5ffaa3a47
Darkness00132 <mustafa54862@gmail.com>
2025-09-30 00:30:46 +0300

    first commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -70,6 +70,18 @@ export default function RootLayout({
       className={geistCairo.className}
       dir="rtl"
     >
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XXXXXXXXXX');
+`}</Script>
+      </head>
       <body className="antialiased bg-gradient-to-bl from-primary/30 via-base-100 to-secondary/30 overflow-x-hidden min-h-screen">
         <Provider>
           <Header />
--- frontend/src/components/UI/Provider.tsx
+++ frontend/src/components/UI/Provider.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
+import GAListener from "./GAListener";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   // create client once per session
@@ -18,6 +19,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
         duration={3000}
       />
       <QueryClientProvider client={queryClient}>
+        <GAListener />
         {children}
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
